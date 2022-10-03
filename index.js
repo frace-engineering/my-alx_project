f246f24be8992feccbc0783e0837d6a6801ab9be
Ugwu Friday B <fugwungene@gmail.com>
2022-10-03 01:18:35 -0700

    Alert message

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-const div = document.querySelectorAll(".box");
-const para = document.createElement("p");
-const paraTxt = document.createTextNode("This is lovely");
-p.appendChild(paraTxt);
-div[1].appendChild(p);
-
+alert("How are you?");
