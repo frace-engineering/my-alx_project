f13bc68e32815321a12f2a39df21df669027c5b4
Ugwu Friday B <fugwungene@gmail.com>
2022-10-03 00:47:48 -0700

    Invoke js

---
+++ index.js
@@ -0,0 +1,6 @@
+const div = document.querySelectorAll(".box");
+const para = document.createElement("p");
+const paraTxt = document.createTextNode("This is lovely");
+p.appendChild(paraTxt);
+div.forEach(box, div.appendChild(p);
+
